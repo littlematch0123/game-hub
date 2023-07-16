4c5b46632c8b120fbf6e635e7840b7d9dd01dc21
xiaohuochai <121631835@qq.com>
2023-07-16 19:24:23 +0800

    feat(component): build the platform icon component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,21 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatFormIconList from "./PlatFormIconList";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
-  const { name, background_image } = game;
+  const { name, background_image: img, parent_platforms } = game;
 
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={background_image} />
+      <Image src={img} />
       <CardBody>
         <Heading size="lg">{name}</Heading>
+        <PlatFormIconList
+          platforms={parent_platforms.map(({ platform }) => platform)}
+        />
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatFormIconList.tsx
@@ -0,0 +1,40 @@
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatFormIconList = ({ platforms }: Props) => {
+  const iconMap: Record<string, IconType> = {
+    pc: FaWindows as IconType,
+    playstation: FaPlaystation as IconType,
+    xbox: FaXbox as IconType,
+    nintendo: SiNintendo as IconType,
+    mac: FaApple as IconType,
+    linux: FaLinux as IconType,
+    ios: MdPhoneIphone as IconType,
+    android: FaAndroid as IconType,
+    web: BsGlobe as IconType,
+  };
+  return (
+    <HStack marginY={2}>
+      {platforms.map(({ id, slug }) => (
+        <Icon key={id} as={iconMap[slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatFormIconList;
