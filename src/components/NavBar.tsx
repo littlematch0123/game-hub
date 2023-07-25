deb9adf6b3c9c92a8b56a20fd1f1b759944ad527
xiaohuochai <121631835@qq.com>
2023-07-25 11:37:22 +0800

    fix: clear all state when click the logo.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,25 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
+import useGameStore from "../store";
 export const NavBar = () => {
+  const setPlatformId = useGameStore((s) => s.setPlatformId);
+  const setGenreId = useGameStore((s) => s.setGenreId);
+  const setSearchText = useGameStore((s) => s.setSearchText);
+  const setSortOrder = useGameStore((s) => s.setSortOrder);
+
   return (
     <HStack as="header" p={2.5}>
-      <Link style={{ flexShrink: 0 }} to="/">
+      <Link
+        style={{ flexShrink: 0 }}
+        to="/"
+        onClick={() => {
+          setPlatformId(undefined);
+          setGenreId(undefined);
+          setSearchText("");
+          setSortOrder("");
+        }}
+      >
         <Image src={logo} boxSize="54px" alt="logo" />
       </Link>
       <SearchInput />
