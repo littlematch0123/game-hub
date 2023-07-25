bcf613e73c0323cb64b941240b698db6858fa41c
xiaohuochai <121631835@qq.com>
2023-07-26 07:33:12 +0800

    fix: navigate to home when submit input value

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,10 +1,15 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameStore from "../store";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
 
 const SearchInput = () => {
+  const navigate = useNavigate();
+  const params = useParams();
   const [inputValue, setInputValue] = useState("");
+  const setGenreId = useGameStore((state) => state.setGenreId);
+  const setPlatformId = useGameStore((state) => state.setPlatformId);
   const setSearchText = useGameStore((state) => state.setSearchText);
   return (
     <InputGroup>
@@ -13,8 +18,20 @@ const SearchInput = () => {
       </InputLeftElement>
       <Input
         value={inputValue}
-        onBlur={() => setSearchText(inputValue)}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            if (params.slug) {
+              setGenreId(undefined);
+              setPlatformId(undefined);
+            }
+            navigate("/");
+            setSearchText(inputValue);
+            setInputValue("");
+          }
+        }}
         borderRadius="20px"
         placeholder="Search games..."
         variant="filled"
