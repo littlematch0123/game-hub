332b5c0330ac8dc88da98e4dfec87ba965c0c784
xiaohuochai <121631835@qq.com>
2023-07-26 07:30:26 +0800

    refactor(logo): reset store and navigate to home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,28 +2,23 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-import { Link } from "react-router-dom";
 import useGameStore from "../store";
+import { useNavigate } from "react-router-dom";
 export const NavBar = () => {
-  const setPlatformId = useGameStore((s) => s.setPlatformId);
-  const setGenreId = useGameStore((s) => s.setGenreId);
-  const setSearchText = useGameStore((s) => s.setSearchText);
-  const setSortOrder = useGameStore((s) => s.setSortOrder);
-
+  const store = useGameStore();
+  const navigate = useNavigate();
   return (
     <HStack as="header" p={2.5}>
-      <Link
-        style={{ flexShrink: 0 }}
-        to="/"
+      <Image
         onClick={() => {
-          setPlatformId(undefined);
-          setGenreId(undefined);
-          setSearchText("");
-          setSortOrder("");
+          store.reset();
+          navigate("/");
         }}
-      >
-        <Image src={logo} boxSize="54px" alt="logo" />
-      </Link>
+        src={logo}
+        boxSize="54px"
+        alt="logo"
+        objectFit="cover"
+      />
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
