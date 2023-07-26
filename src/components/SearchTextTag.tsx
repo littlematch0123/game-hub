cb0d30928276804bdd9e8d95f6ac8a4bcf54abd5
xiaohuochai <121631835@qq.com>
2023-07-26 12:30:10 +0800

    fix: break word between long number or word.

--- src/components/SearchTextTag.tsx
+++ src/components/SearchTextTag.tsx
@@ -11,7 +11,7 @@ const SearchTextTag = () => {
         setSearchText("");
       }}
     >
-      <TagLabel>{searchText}</TagLabel>
+      <TagLabel wordBreak="break-all">{searchText}</TagLabel>
       <TagCloseButton />
     </Tag>
   );
